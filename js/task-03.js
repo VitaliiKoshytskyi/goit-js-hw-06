25568cc4d7cb890ef75f21939007d1b1609ec438
Vitalii Koshytskyi <115081482+VitaliiKoshytskyi@users.noreply.github.com>
2023-01-07 11:47:14 +0200

    updated task #3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,9 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryListEL = document.querySelector('.gallery')
+console.log(galleryListEL)
+const markup = images.map(element => `<li><img class='gallery' src =${element.url} alt=${element.alt} width = 300></li>`).join('')
+console.log(markup)
+galleryListEL.insertAdjacentHTML("beforeend",markup)
