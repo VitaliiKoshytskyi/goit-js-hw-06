10dcee86f850449269a456077308bc41e456792f
Vitalii Koshytskyi <115081482+VitaliiKoshytskyi@users.noreply.github.com>
2023-01-07 14:07:51 +0200

    updated task 4

---
+++ js/task-04.js
@@ -0,0 +1,30 @@
+let counterValue = 0;
+const butttonDecrementEl = document.querySelector('[data-action="decrement"]')
+const butttonIncrementEl = document.querySelector('[data-action="increment"]')
+const counterValueEl = document.querySelector('#value')
+
+console.log(butttonDecrementEl)
+console.log(butttonIncrementEl)
+console.log(counterValueEl)
+
+
+
+const increaseNumberHandler = () => {
+    console.log("click vie increase button")
+    counterValue += 1
+     counterValueEl.textContent = counterValue
+    return counterValueEl
+}
+    
+
+
+const decreaseNumberHandler = () => {
+    console.log("click vie decrease button")
+    counterValue -= 1
+    counterValueEl.textContent = counterValue
+    return counterValueEl
+}
+    
+
+butttonDecrementEl.addEventListener('click',decreaseNumberHandler )
+butttonIncrementEl.addEventListener('click',increaseNumberHandler )
