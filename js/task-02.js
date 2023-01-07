2de8fc113a5962790ae5b482b0eef7dfdbd3d192
Vitalii Koshytskyi <115081482+VitaliiKoshytskyi@users.noreply.github.com>
2023-01-07 10:46:10 +0200

    updated task #2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsListEl = document.querySelector('#ingredients')
+console.log(ingredientsListEl)
+
+ingredients.forEach(elem => {
+  const list = document.createElement('li')
+  list.textContent = elem
+  list.classList.add('item')
+  ingredientsListEl.append(list)
+
+})
