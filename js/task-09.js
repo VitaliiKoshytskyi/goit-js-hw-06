9f54dd9b1b07ab793899a1e800f63a34e2454344
Vitalii Koshytskyi <115081482+VitaliiKoshytskyi@users.noreply.github.com>
2023-01-08 20:34:09 +0200

    updated task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const bodyEl = document.querySelector('body')
+const buttonEl = document.querySelector('.change-color')
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const changeBkgColotHandler = () => {
+  bodyEl.style.backgroundColor = getRandomHexColor()
+}
+
+buttonEl.addEventListener('click', changeBkgColotHandler)
+
+console.log(getRandomHexColor())
