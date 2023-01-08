513daf39ec2e5869a1064ab1cd415b84cade3e32
Vitalii Koshytskyi <115081482+VitaliiKoshytskyi@users.noreply.github.com>
2023-01-08 20:16:40 +0200

    updated task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const loginFormEl = document.querySelector('.login-form')
 
+
+const submitHandler = (event) => {
+    
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+    if (email.value === "" || password.value === "") {
+        return alert("Fill in all the fields!");
+    }
+      console.log(`Login: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
+
+
+loginFormEl.addEventListener('submit',submitHandler)
