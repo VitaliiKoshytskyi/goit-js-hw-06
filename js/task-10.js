a6fe38b7f8e2b5157fd0e19a1c751656b221ca83
Vitalii Koshytskyi <115081482+VitaliiKoshytskyi@users.noreply.github.com>
2023-01-10 11:41:13 +0200

    updated task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,37 @@
+const inputEl = document.querySelector('input')
+const btnCreateEl = document.querySelector('[data-create]')
+const btnDescroyEl = document.querySelector('[data-destroy]')
+const divEl = document.querySelector('#boxes')
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+function createBoxes() {
+  let newArray = []
+  let inputValues = inputEl.value
+  let widthHeight = 30
+ 
+  for (let i = 0; i < inputValues;i+=1) {
+    let divCreate =document.createElement('div')
+    divCreate.style.width = `${widthHeight}px`
+    divCreate.style.height = `${widthHeight}px`
+    divCreate.style.backgroundColor = getRandomHexColor()
+    widthHeight+=10
+    newArray.push(divCreate)
+  }
+  divEl.append(...newArray)
+ 
+  
+}
+
+function destroyBoxes() {
+divEl.innerHTML = ''
+}
+
+
+
+btnCreateEl.addEventListener('click', createBoxes)
+btnDescroyEl.addEventListener('click', destroyBoxes)
+
+
